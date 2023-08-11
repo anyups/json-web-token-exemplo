88a5c7180183fc1fdc05bca6ea433cdbd6f7be21
any <anyy.gabriele@gmail.com>
2023-08-11 17:14:45 -0400

    commit 5

--- servidor/index.js
+++ servidor/index.js
@@ -25,7 +25,7 @@ app.use(
     secret: process.env.SECRET,
     algorithms: ["HS256"],
     getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/"] })
+  }).unless({ path: ["/autenticar", "/logar", "/deslogar"] })
 );
 
 app.get('/autenticar', async function(req, res){
@@ -38,14 +38,28 @@ app.get('/', async function(req, res){
 
 app.post('/logar', (req, res) => {
   if (req.body.usuario == "any" && req.body.senha == 9804) {
-    res.send("você está logado!")
-  } else {
-    res.send("cade o cadastro?")
+    const id = 2;
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 300
+    })
+
+    res.cookie('token', token, { httpOnly: true });
+    return res.json({
+      usuario: req.body.usuario,
+      token: token
+    })
   }
+
+  res.status(500).json({
+    mensagem: "login inválido"
+  })
 })
 
 app.post('/deslogar', function(req, res) {
-  
+  res.cookie('token', null, { httpOnly: true });
+  res.json({
+    deslogado: true
+  })
 })
 
 app.listen(3000, function() {
